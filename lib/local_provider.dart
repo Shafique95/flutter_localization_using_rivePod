dd8ae18d9d83d61925d490f1ee19924b7498e752
Shafiqul Islam <www.shafiquebd@gmail.com>
2023-01-03 13:59:51 +0600

    localization Project

---
+++ lib/local_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:localization_flutter/shared_pref.dart';
+
+import 'supported_locale.dart';
+
+final localeProvider = StateNotifierProvider<LocaleNotifier, Locale>((ref) {
+  return LocaleNotifier();
+});
+
+class LocaleNotifier extends StateNotifier<Locale> {
+  LocaleNotifier() : super(Locale('en')) {
+    onAppStart();
+  }
+
+  void changeLanguage(SupportedLocale locale) {
+    try {
+      AppSharedPreference.saveLocale(locale);
+      state = Locale(locale.code);
+    } catch (error) {
+      state = Locale('en');
+    }
+  }
+
+  void onAppStart() {
+    try {
+      final locale = AppSharedPreference.getLocale();
+      state = locale;
+    } catch (error) {
+      state = Locale('en');
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:localization_flutter/shared_pref.dart';
+import 'localization_helper.dart';
+import 'local_provider.dart';
+import 'supported_locale.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppSharedPreference.init();
+  runApp(ProviderScope(child: LocalizationDemo()));
+}
+
+class LocalizationDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (BuildContext context, WidgetRef ref, Widget? child) {
+        final locale = ref.watch(localeProvider);
+        return MaterialApp(
+          title: 'Localization Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          locale: locale,
+          supportedLocales: [
+            Locale('en', 'US'),
+            Locale('fr', 'FR'),
+            Locale('ar'),
+          ],
+          localizationsDelegates: [
+            GlobalCupertinoLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            LocalizationHelper.delegate
+          ],
+          home: child,
+        );
+      },
+      child: HomePage(),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  static const imageLocation = 'images/home_image.png';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${LocalizationHelper.of(context)?.appName}'),
+        actions: [
+          PopupMenuButton<SupportedLocale>(
+            itemBuilder: (context) {
+              return SupportedLocale.values
+                  .map<PopupMenuEntry<SupportedLocale>>((e) => PopupMenuItem(
+                        child: Text('${e.name}'),
+                        value: e,
+                      ))
+                  .toList();
+            },
+            onSelected: (locale) {
+              ref.read(localeProvider.notifier).changeLanguage(locale);
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Spacer(
+              flex: 3,
+            ),
+            Image.asset(
+              imageLocation,
+              fit: BoxFit.cover,
+            ),
+            Spacer(),
+            Text('${LocalizationHelper.of(context)?.description}',
+                textAlign: TextAlign.start,
+                style: Theme.of(context).textTheme.headline5),
+            Spacer(
+              flex: 3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
